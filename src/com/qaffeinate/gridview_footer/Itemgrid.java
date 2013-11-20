476455d567842397d54b1b890a57f101f6d52207
jitender chaudhary <29jitender@gmail.com>
2013-11-20 23:59:45 +0530

    first commit

---
+++ src/com/qaffeinate/gridview_footer/Itemgrid.java
@@ -0,0 +1,23 @@
+package com.qaffeinate.gridview_footer;
+
+
+ 
+
+public class Itemgrid {
+ 	private String url;
+
+	public Itemgrid(String string, String title) {
+		super();
+		this.url = string;
+ 	}
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	 
+	
+
+}
